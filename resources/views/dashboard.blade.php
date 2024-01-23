0abdaeb6498635e367170f75c22516e87456a332
Arkios363 <kiko54585@gmail.com>
2024-01-23 19:37:44 +0100

    Enllaç a la blade destinos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,12 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+                    
+                    <!-- Agrega el botón de enlace a la ruta /destinos -->
+                    <a href="{{ url('/destinos') }}" class="text-blue-500 hover:underline">Go to Destinos</a>
+
+                    <!-- O usa el componente de enlace de Laravel -->
+                    {{-- <x-link :href="url('/destinos')" class="text-blue-500 hover:underline">Go to Destinos</x-link> --}}
                 </div>
             </div>
         </div>
